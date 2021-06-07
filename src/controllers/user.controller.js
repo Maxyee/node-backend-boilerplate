db1dbf94ec398a3af8cd730b1fd8884f5b9d72fa
maxyee <eyaminkhan00@gmail.com>
2021-06-07 16:46:25 +0600

    completed the crud operation for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -55,7 +55,93 @@ exports.createUser = async (req,res)=> {
         return res.status(500).send({
             message: `Error: ${err.message}`
         })
+    }    
+}
+
+
+exports.deleteUser = async (req, res) => {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).send({
+        message: 'Please provide a id for the user you are trying to delete!',
+      });
+    }
+  
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+  
+    if (!user) {
+      return res.status(400).send({
+        message: `No user found with the id ${id}`,
+      });
+    }
+  
+    try {
+      await user.destroy();
+      return res.send({
+        message: `User ${id} has been deleted!`,
+      });
+    } catch (err) {
+      return res.status(500).send({
+        message: `Error: ${err.message}`,
+      });
+    }
+};
+  
+
+
+exports.updateUser = async (req,res)=> {
+    const {userName,firstName,lastName,address,email,password} = req.body;
+    const { id } = req.params;
+
+    const user = await User.findOne({
+        where: {
+          id,
+        },
+    });
+
+    if (!user) {
+        return res.status(400).send({
+          message: `No user found with the id ${id}`,
+        });
     }
 
+    try {
+        if (userName) {
+          user.username = userName;
+        }
+        if (firstName) {
+          user.firstName = firstName;
+        }
+
+        if (lastName) {
+            user.lastName = lastName;
+        }
+
+        if (address) {
+            user.address = address;
+        }
+
+        if (email) {
+            user.email = email;
+        }
+        if (password) {
+            user.password = password;
+        }
     
+        user.save();
+        return res.send({
+          message: `User ${id} has been updated!`,
+        });
+      } catch (err) {
+        return res.status(500).send({
+          message: `Error: ${err.message}`,
+        });
+    }
+ 
 }
+
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,8 +5,10 @@ const router = Router();
 
 
 // Routes go here
-router.get('/:id', user.getUser )
-router.post('/createUser', user.createUser)
+router.get('/:id', user.getUser );
+router.post('/createUser', user.createUser);
+router.put('/updateUser/:id', user.updateUser);
+router.delete('/deleteUser', user.deleteUser);
 
 
 export default router;
